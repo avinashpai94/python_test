b632e5262c9edda03edb6791c67a4e1e536718ca
avinashpai94 <avinashpai94@gmail.com>
2018-08-21 00:30:57 -0700

    new test

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-import  bitlyapi
-import  sys
+import random
 
-API_USER = "o_1bqf5gdhav"
-API_KEY = "R_e3ec11466903417bb8da9ab27310550f"
+randnum = random.randint(1,10)
+guess = 0
 
-b = bitlyapi.BitLy(API_USER,API_KEY)
-
-usage = """Usage: python shortener.py [url] e.g python shortener.py http://www.google.com"""
-
-if len(sys.argv)!=2:
-    print(usage)
-    sys.exit(0)
-
-longurl = sys.argv[1]
-
-response = b.shorten(longurl=longurl)
-print(response['url'])
+while randnum != guess:
+    guess = int(input("\nEnter a Number"))
+    if guess > randnum:
+        print("Too high")
+    elif guess < randnum:
+        print("Too low")
+    else:
+        print("Correct")
