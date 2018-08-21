c4f2a4098cbb2b0dad4da94fd714cca27e8da86d
avinashpai94 <avinashpai94@gmail.com>
2018-08-21 15:13:41 -0700

    Initial Python project - Hangman
    Hangman
    First python project to learn
    Keeps track of guesses
    Fills in blanks to show remaining guesses

--- test.py
+++ test.py
@@ -1,13 +1,47 @@
-import random
+from random_words import RandomWords
+import regex as re
 
-randnum = random.randint(1,10)
-guess = 0
 
-while randnum != guess:
-    guess = int(input("\nEnter a Number"))
-    if guess > randnum:
-        print("Too high")
-    elif guess < randnum:
-        print("Too low")
-    else:
-        print("Correct")
+def fetch_word():
+    rw = RandomWords()
+    word = rw.random_word()
+    return word
+
+
+def replaceall(positions, guesschar, guess):
+    guess = list(guess)
+    for i in positions:
+        guess[i] = guesschar
+    guess = ''.join(guess)
+    return guess
+
+
+def main():
+    word = fetch_word()
+    guess = "-" * len(word)
+    remaining = 7
+    print(word)
+    alreadyguessed = []
+    while "-" in guess:
+        positions = []
+        print(guess)
+        guesschar = input("Enter a Character")
+        positions = [i for i, x in enumerate(word) if x == guesschar]
+        if guesschar in alreadyguessed:
+            print("You have already guessed %s" % guesschar)
+        else:
+            if not positions:
+                remaining = remaining - 1
+                print("Wrong! %s guesses remain" % remaining)
+            else:
+                guess = replaceall(positions, guesschar, guess)
+
+            if remaining == 0:
+                print("You lose! The word is %s" % word)
+                exit(0)
+        alreadyguessed.append(guesschar)
+    print("You guessed it! The word is %s" % word)
+
+
+if __name__ == '__main__':
+    main()
